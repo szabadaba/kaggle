a3cfbf0cec3f8da14da19ea1c5922054e0a05b14
szabadaba <szabados@mybasis.com>
2015-03-09 18:26:15 -0700

    dev

--- MMLM2015/create_ncaa_training_set.py
+++ MMLM2015/create_ncaa_training_set.py
@@ -1,7 +1,10 @@
 __author__ = 'Steven Szabados'
 
+from joblib import Parallel, delayed
+import multiprocessing
 import pandas as ps
 import numpy as np
+from math import sqrt
 import sys
 
 
@@ -11,23 +14,69 @@ class CreateNCAATrainingSet:
     def __init__(self):
         # load seson data
         data = ps.io.parsers.read_csv(season_detail_filename)
+        self.game_stats = []
 
         # break up data into seasons
         s_data = []
         for season in data.season.unique():
             s_data.append(data[data.season == season])
 
+        for season in s_data:
+            self.game_stats.append(CreatePreGameStats())
+            print season.irow(0).season
+            for game in season.iterrows():
+                self.game_stats[-1].add_game(game[1], season)
+
+            s_win_prob = np.asarray(self.game_stats[-1].w_team_prob)
+            # s_win_prob2 = np.asarray(self.game_stats[-1].w_team_prob2)
+            print float(sum(s_win_prob > 1.0))/len(s_win_prob)
+            # print float(sum(s_win_prob2 > 1.0))/len(s_win_prob2)
+
+        # for game in s_data[0].iterrows():
+        #     self.game_stats.add_game(game[1], s_data[0])
+
 
 class CreatePreGameStats:
-    def __init__(self, game, season):
-        MIN_DAYS = 30
+    def __init__(self):
+        self.MIN_DAYS = 30
+        self.pts_rank_results = []
+        self.ptr_rank_daynum = []
+        self.w_team_prob = []
+        # self.w_team_prob2 = []
+
+    def add_game(self, game, season):
 
         # create first game day
         daynum1 = min(season.daynum)
 
         # do we have enough days?
-        if (game.daynum - daynum1) > MIN_DAYS:
+        if (game.daynum - daynum1) > self.MIN_DAYS:
+            # create games
             eval_games = season[season.daynum < game.daynum]
+
+            # do we not have this ranking?
+            if game.daynum not in self.ptr_rank_daynum:
+                pts_rank = CreateWeightedPtsRanking(eval_games)
+                if pts_rank.success:
+                    self.pts_rank_results.append(pts_rank)
+                    self.ptr_rank_daynum.append(game.daynum)
+
+            # can we create the points ranking?
+            if game.daynum in self.ptr_rank_daynum:
+                pts_rank = self.pts_rank_results[self.ptr_rank_daynum.index(game.daynum)]
+                # did we get a ranking for both teams:
+                if game.wteam in pts_rank.teams and game.lteam in pts_rank.teams:
+                    w_team_rank = pts_rank.team_rank[pts_rank.teams.index(game.wteam)]
+                    l_team_rank = pts_rank.team_rank[pts_rank.teams.index(game.lteam)]
+                    # print float(w_team_rank/l_team_rank)
+                    self.w_team_prob.append(float(w_team_rank/l_team_rank))
+
+                    # w_team_w_perc = float(sum(eval_games.wteam == game.wteam))/float((sum(eval_games.wteam == game.wteam) + sum(eval_games.lteam == game.wteam)))
+                    # l_team_w_perc = float(sum(eval_games.wteam == game.lteam))/float((sum(eval_games.wteam == game.lteam) + sum(eval_games.lteam == game.lteam)))
+                    # self.w_team_prob2.append(float((w_team_w_perc + 0.5)/(l_team_w_perc + 0.5)))
+
+
+
 
         # self.team = team
         # self.pts_rank = pts_rank
@@ -44,9 +93,9 @@ class CreateWeightedPtsRanking:
         for row in games.iterrows():
             game = row[1]
             if game.wteam not in teams:
-                teams.append(game.team)
+                teams.append(game.wteam)
             if game.lteam not in teams:
-                teams.append(game.team)
+                teams.append(game.lteam)
 
         # create pts matrix
         point_mtx = np.zeros((len(teams), len(teams)))
@@ -63,7 +112,8 @@ class CreateWeightedPtsRanking:
             l_idx = teams.index(game.lteam)
 
             # create dampen factor
-            dampen_factor = (1.0/(max_daynum - game.daynum + 1))
+            dampen_factor = sqrt((1.0/(max_daynum - game.daynum + 1)))
+            # dampen_factor = 1
 
             # add game to count
             numb_games[w_idx] += 1*dampen_factor
@@ -72,15 +122,21 @@ class CreateWeightedPtsRanking:
             # add points to mtx
             point_mtx[w_idx, l_idx] += dampen_factor*float(row[1].wscore)/(float(row[1].wscore + row[1].lscore))
             point_mtx[l_idx, w_idx] += dampen_factor*float(row[1].lscore)/float((row[1].wscore + row[1].lscore))
+            # point_mtx[w_idx, l_idx] += dampen_factor*float(row[1].wscore)
+            # point_mtx[l_idx, w_idx] += dampen_factor*float(row[1].lscore)
 
         # scale points matrix
         d = np.diag(1.0/numb_games)
-        point_mtx_scale = np.dot(d, self.point_mtx)
+        point_mtx_scale = np.dot(d,point_mtx)
 
         # make sure the matrix is not singular
         if np.linalg.cond(point_mtx_scale) < 1/sys.float_info.epsilon:
-            v = np.asarray(np.linalg.eig(point_mtx_scale)[1].real).tolist
-            w_team_rank = v(teams.index(game.wteam))
+            V = np.linalg.eig(point_mtx_scale)[1]
+            self.team_rank = abs(np.asarray(V[:, 0].real).transpose()).tolist()
+            self.teams = teams
+            self.success = True
+        else:
+            self.success = False
 
 
 
--- MMLM2015/test_script.py
+++ MMLM2015/test_script.py
@@ -3,12 +3,15 @@ __author__ = 'Steve'
 import load_mmlm_data as ld
 import load_season_data as ls
 import aggregate_ncaa_season_data as ag
+import create_ncaa_training_set as nc
 
 import analyze_cbb_season as an
 
+t = nc.CreateNCAATrainingSet()
+
 # t = ld.load_data()
-t = ls.LoadSeasonData()
+# t = ls.LoadSeasonData()
 
 # r = an.point_matrix(t.seasons[-2], t.teams)
-r = ag.RankByPoints(t.seasons[-1], t.teams)
+# r = ag.RankByPoints(t.seasons[-1], t.teams)
 x = 1
